43e97f6857174e907d3097b4540dc07e8de37fe1
drozi26 <oguzhannnsozennn1997@gmail.com>
2024-12-05 19:51:51 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,18 +1,17 @@
 # Journey details (check stations.json for valid station names)
-binis_istasyon_adi = "İstanbul(Söğütlüçeşme)"
-inis_istasyon_adi = "Ankara Gar"
-date = "2024-02-02"
+binis_istasyon_adi = "Eskişehir"
+inis_istasyon_adi = "İzmit YHT"
 
 # If you want to check all journeys in a given date/day, set to False. 
 # Else set to True and give a specific valid journey hour.
-check_specific_hour = False
-hour = "08:02"
+check_specific_hour = True
+hour = "20:24"
 
 # Email details (replace with the email address (must be OUTLOOK) and password that will send notification
 # and email addres that will be notified)
-email_address = "sender_mail@outlook.com" 
-email_password = "sender_password"
-destination_address = "receiver_mail@any_mail.com"
+email_address = "oguzhan.sozen26@outlook.com" 
+email_password = "Apollo21."
+destination_address = "oguzhansozen1997@hotmail.com"
 
 # Wait time between checks in seconds
 sleep_time = 10
